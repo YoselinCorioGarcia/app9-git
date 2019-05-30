1461cff09f358f747d9c610c67b0381f9a7411c1
Yoselin Corio Garcia <marinettedupaincheng096@gmail.com>
2019-05-30 14:38:39 -0500

    4 subida

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ var audio;
 //Hide Pause Initially
 $('#pause').hide();
 
-//Initializer_Play First Song
-iniAudio($('#playlist lo:first-child'));
+//Initializer-Play First Song
+initAudio($('#playlist li:first-child'));
 
 function initAudio(element){
-	var song=element.attr('song');
+	var song = element.attr('song');
 	var title = element.text();
 	var cover = element.attr('cover');
 	var artist = element.attr('artist');
 	
 	//Create a New Audio Object
-	audio = new Audio ('media/' + song);
+	audio = new Audio('media/' + song);
 	
 	if(!audio.currentTime){
 		$('#duration').html('0.00');
@@ -31,23 +31,23 @@ function initAudio(element){
 
 
 //Play Button
-$('#play').click(function (){
+$('#play').click(function(){
 	audio.play ();
 	$('#play').hide();
 	$('#pause').show();
-	$('#duration').fadeln(400);
+	$('#duration').fadeIn(400);
 	showDuration();
 });
 
 //Pause Button
 $('#pause').click(function (){
-	audio.pause();
+	audio.pause ();
 	$('#pause').hide();
 	$('#play').show();
 });
 
-//Stop Button
-$('#stop').click (function (){
+//Stop Button 
+$('#stop').click(function (){
 	audio.pause ();
 	audio.currentTime = 0;
 	$('#pause').hide();
@@ -57,35 +57,35 @@ $('#stop').click (function (){
 
 //Playlist Song Click
 $('#playlist li').click(function (){
-	audio.pause();
+	audio.pause ();
 	initAudio($(this));
-	$('#play').hide();
-	$('#pause').show();
-	$('#duration').fadeOut(400);
-	audio.play();
-	showDuration();
+	$('#pause').hide();
+	$('#play').show();
+	$('#duration').fadeIn(400);
+audio.play();
+showDuration();
 });
 
 //Volume Control
 $('#volume').change(function (){
-	audio.volume = parseFloat (this.value/ 10);
+	audio.volume = parseFloat(this.value / 10);
 });
-
-//Time Duration
-function showDuration(){
-		$(audio).bind('timeupdate', function(){
-			//Get hours and minutes
+	
+	//Time Duration
+	function showDuration (){
+		$(audio).bind('timeupdate',function (){
+			//Get hours and minutes 
 			var s = parseInt(audio.currentTime % 60);
-			var m = parseInt((audio.currentTime / 60) % 60);
-			//Add 0 if seconds less than 10
+			var m = parseInt((audio.currentTime / 60)% 60);
+			//Add 0 of seconds less than 10
 			if (s < 10){
-				s ='0' + s;
+				s= '0' + s;
 			}
 			$('#duration').html(m + '.' + s);
 			var value = 0;
-			if (audio.currentTime > 0){
-				value = Math.floor((100/ audio.duarion) * audio.currentTime);
+			if (audio.currentTime > 0) { 
+			value = Math.floor((100/audio.duration)*audio.currentTime);
 			}
 			$('#progress').css('width',value+'%');
 		});
-}
+	}
